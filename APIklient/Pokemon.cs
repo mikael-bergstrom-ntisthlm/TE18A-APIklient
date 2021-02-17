b06062a88c421a2d38b0181167d5a7fb826f69e3
Mikael <mikael.bergstrom@ga.ntig.se>
2021-02-17 11:08:56 +0100

    Initial commit

---
+++ APIklient/Pokemon.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APIklient
+{
+  public class Pokemon
+  {
+    public string name;
+    public int weight;
+    public int base_experience;
+  }
+}
---
+++ APIklient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace APIklient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+
+      RestRequest request = new RestRequest("pokemon/grejsimojs");
+      IRestResponse response = client.Get(request);
+
+      Console.WriteLine(response.StatusCode);
+
+      if (response.StatusCode == System.Net.HttpStatusCode.OK)
+      {
+        Pokemon thePokemon = JsonConvert.DeserializeObject<Pokemon>(response.Content);
+
+        Console.WriteLine(thePokemon.name);
+      }
+
+
+      Console.ReadLine();
+    }
+  }
+}
